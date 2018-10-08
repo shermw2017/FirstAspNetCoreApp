603895b47422791f4dc7cb19ddc46684e0b5adee
shermw <shermw@microsoft.com>
2018-10-08 10:12:42 -0700

    Add project files.

---
+++ FirstAspNetCoreApp/Services/IInventoryServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAspNetCoreApp.Models;
+
+namespace FirstAspNetCoreApp.Services
+{
+    public interface IInventoryServices
+    {
+        InventoryItems AddInventoryItems(InventoryItems items);
+        Dictionary<string, InventoryItems> GetInventoryItems();
+    }
+}
---
+++ FirstAspNetCoreApp/Services/InventoryServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAspNetCoreApp.Models;
+
+namespace FirstAspNetCoreApp.Services
+{
+    public class InventoryServices : IInventoryServices
+    {
+        private readonly Dictionary<string, InventoryItems> _inventoryItems;
+
+        public InventoryServices()
+        {
+            _inventoryItems = new Dictionary<string, InventoryItems>();
+        }
+
+        InventoryItems IInventoryServices.AddInventoryItems(InventoryItems items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+
+            return items;
+        }
+
+        Dictionary<string, InventoryItems> IInventoryServices.GetInventoryItems()
+        {
+            return _inventoryItems;
+
+        }
+    }
+}
